e89be50e481ef1f96d058276c678e8813d283043
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-05-18 08:51:54 +0530

    helm-expense

--- docker.sh
+++ docker.sh
@@ -29,3 +29,7 @@ mv /tmp/eksctl /usr/local/bin
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 curl -sS https://webinstall.dev/k9s | bash
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
